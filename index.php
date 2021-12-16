cee336567a827133de30b7266b37af79f983d16d
Bonu93 <luca.morucci.kb@gmail.com>
2021-12-16 15:02:53 +0100

    complete

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
 
+$paragrafo = 'lorem ipsum dolor sit amet quam';
+
+echo $paragrafo;
+
+echo '<br> lunghezza:'.strlen($paragrafo);
+
+$badword = $_GET['badword'];
+
+$replaced = str_replace($badword, '***', $paragrafo);
+
+echo '<br>'.$replaced;
+
+echo '<br> lunghezza:'.strlen($replaced)
 
 
 
